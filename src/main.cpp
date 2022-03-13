7444842778f9daf68216188e28bc2c677cf369d7
Vlad Slivilescu <vlad.slivilescu@gmail.com>
2022-03-13 14:54:58 +0200

    Remove unnecessary comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,70 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-
-#include "Vector2f.hpp"
+#include <fstream>
+#include <cmath>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-// settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+struct Vector2f {
+    float x;
+    float y;
+    Vector2f(float x, float y): x(x), y(y) {};
+    void print(std::ostream& out) {
+        out << "(" << this->x << ", " << this->y << ")";
+    }
+};
+
+struct Vector3f {
+    float x;
+    float y;
+    float z;
+    Vector3f(float x, float y, float z): x(x), y(y), z(z) {}
+    void print(std::ostream& out) {
+        out << "(" << this->x << ", " << this->y << ", " << this->z << ")";
+    }
+};
+
+
+void draw_triangle(Vector2f x, Vector2f y, Vector2f z, Vector3f color) {
+    glBegin(GL_TRIANGLES);
+        glColor3f(color.x, color.y, color.z);
+        glVertex2f(x.x, x.y);
+        glVertex2f(y.x, y.y);
+        glVertex2f(z.x, z.y);
+    glEnd();
+}
+
+void draw_circle(Vector2f center, float radius, Vector3f color, int steps = 1000) {
+    for (int step = 0; step < steps; ++step) {
+        float angle = (2 * M_PI * step) / steps;
+        float next_angle = (2 * M_PI * (step + 1)) / steps;
+
+        float x1 = radius * sin(angle); 
+        float y1 = radius * cos(angle); 
+
+        float x2 = radius * sin(next_angle); 
+        float y2 = radius * cos(next_angle); 
+
+        draw_triangle(center, Vector2f(x1, y1), Vector2f(x2, y2), color);
+    }
+}
+
+void draw_rectangle(Vector2f p1, Vector2f p2, Vector3f color) {
+    draw_triangle(p1, Vector2f(p2.x, p1.y), p2, color);
+    draw_triangle(p1, Vector2f(p1.x, p2.y), p2, color);
+}
 
 int main()
 {
-    // glfw: initialize and configure
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
 
-    // glfw window creation
-    // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -33,61 +76,36 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    // glad: load all OpenGL function pointers
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }    
 
-    // render loop
-    // -----------
     while (!glfwWindowShouldClose(window))
     {
-        // input
-        // -----
         processInput(window);
 
-        // render
-        // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glLineWidth(1.0f);
-    		glBegin(GL_LINE_LOOP);
-          glColor3f(1.0f, 0.9f, 0.8f);
-          glVertex2f(-0.5f, 0.0f);
-          glVertex2f(0.5f, 0.0f);
-          glVertex2f(0.5f, -1.0f);
-          glVertex2f(-0.5f, -1.0f);
-        glEnd();
+        draw_circle(Vector2f(0, 0), 0.5, Vector3f(0, 1, 1));
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-        // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
     glfwTerminate();
     return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
 }
